79dd44bd01a165c64b347c375affc6baa5b726d2
Joshua Kennedy <josh@outthinkgroup.com>
2020-08-21 09:11:59 -0400

    basic search functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, graphql } from 'gatsby'
 import PostCard from '../components/PostCard'
 import Bio from '../components/Bio'
@@ -7,6 +7,31 @@ import SEO from '../components/seo'
 
 function BlogIndex({ data, location }) {
   const siteTitle = data.site.siteMetadata.title
+  const [searchString, setSearchString] = useState('')
+  function search(needle, haystack) {
+    return haystack.toLowerCase().includes(needle.toLowerCase())
+  }
+  function filterFn(post) {
+    if (searchString.length < 2) return true
+    const {
+      frontmatter: { title, tags },
+    } = post
+    if (search(searchString, title)) {
+      return true
+    }
+
+    if (tags) {
+      for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i]
+        if (tag.toLowerCase().includes(searchString.toLowerCase())) {
+          console.log(post)
+          console.log(tag)
+          return true
+        }
+      }
+    }
+  }
+
   const { allMdx } = data
   return (
     <Layout location={location} title={siteTitle}>
@@ -15,9 +40,16 @@ function BlogIndex({ data, location }) {
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-
+        <form onSubmit={e => e.preventDefault()}>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            onChange={e => setSearchString(e.target.value)}
+          />
+        </form>
         <ul className="post-list">
-          {allMdx.nodes.map(post => {
+          {allMdx.nodes.filter(filterFn).map(post => {
             return (
               <li key={post.fields.slug}>
                 <PostCard post={post} />
